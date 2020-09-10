6200f23dcfbc481f6c95d628e5519f59dfc6e79a
Robin Drexler <robin.drexler@shopify.com>
2020-09-10 12:42:33 -0400

    Add extension.config.yml to generated project

--- scripts/generate/clean-up.ts
+++ scripts/generate/clean-up.ts
@@ -10,6 +10,7 @@ export function cleanUp() {
   delete json.scripts.generate;
   delete json.scripts['clean-up'];
   delete json.devDependencies['@types/yargs'];
+  delete json.devDependencies['fs-extra'];
   delete json.devDependencies['inquirer'];
   delete json.devDependencies['ts-node'];
   delete json.devDependencies['yargs'];
