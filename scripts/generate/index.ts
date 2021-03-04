ca19ffce822e014c6536a50a8fcaa3a2ccfe3258
Henry Tao <henry.tao@shopify.com>
2021-03-04 10:16:54 -0500

    Support template arg

--- scripts/generate/index.ts
+++ scripts/generate/index.ts
@@ -5,8 +5,43 @@ import {cleanUp} from './clean-up';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const inquirer = require('inquirer');
 
+function validateTemplateIdentifier(templateIdentifier: string): Template {
+  if (isTemplate(templateIdentifier)) {
+    return templateIdentifier;
+  }
+  throw new Error(`Unknown template: ${templateIdentifier}`);
+}
+
+function isTemplate(
+  templateIdentifier: string
+): templateIdentifier is Template {
+  for (const key in Template) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if ((Template as any)[key] === templateIdentifier) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function getTemplateIdentifier() {
+  const response = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'template',
+      message: 'Select template:',
+      min: 1,
+      max: 1,
+      instructions: false,
+      choices: Object.values(Template),
+    },
+  ]);
+  const {template} = response;
+  return template;
+}
+
 (async () => {
-  const {type: extensionPoint} = yargs.argv;
+  const {type: extensionPoint, template: templateIdentifier} = yargs.argv;
   console.log('Create ', extensionPoint, ' extension project');
   if (!extensionPoint) {
     console.error(
@@ -19,19 +54,9 @@ See README.md for instructions.
     return;
   }
 
-  const response = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'template',
-      message: 'Select template:',
-      min: 1,
-      max: 1,
-      instructions: false,
-      choices: ['vanilla', 'react', 'vanilla-typescript', 'react-typescript'],
-    },
-  ]);
-
-  const {template} = response;
+  const template = templateIdentifier
+    ? validateTemplateIdentifier(templateIdentifier as string)
+    : await getTemplateIdentifier();
 
   console.log('✅ You selected:', template);
 
